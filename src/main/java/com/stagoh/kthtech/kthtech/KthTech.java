a9e2d50013dabaf5f7b2c11229ba245b1e2873d0
StableAgOH <stagoh17@gmail.com>
2022-12-22 20:01:14 +0800

    feat: init kthtech

---
+++ src/main/java/com/stagoh/kthtech/kthtech/KthTech.java
@@ -0,0 +1,15 @@
+package com.stagoh.kthtech.kthtech;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(KthTech.MODID)
+public class KthTech {
+    public static final String MODID = "kthtech";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public KthTech() {
+        LOGGER.info("KthTech");
+    }
+}

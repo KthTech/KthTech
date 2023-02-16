7c8fe9527fbeae0c3542918289a855a4bacb442e
StableAgOH <stagoh@qq.com>
2023-02-17 00:48:12 +0800

    style: organize imports

--- src/main/java/com/stagoh/kthtech/kthtech/KthTech.java
+++ src/main/java/com/stagoh/kthtech/kthtech/KthTech.java
@@ -1,6 +1,7 @@
 package com.stagoh.kthtech.kthtech;
 
 import org.slf4j.Logger;
+
 import com.mojang.logging.LogUtils;
 
 import net.minecraftforge.fml.common.Mod;
--- src/main/java/com/stagoh/kthtech/kthtech/block/entity/KTCrusherBlockEntity.java
+++ src/main/java/com/stagoh/kthtech/kthtech/block/entity/KTCrusherBlockEntity.java
@@ -2,8 +2,8 @@ package com.stagoh.kthtech.kthtech.block.entity;
 
 import com.stagoh.kthtech.kthtech.registry.KTBlockEntityTypes;
 
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;

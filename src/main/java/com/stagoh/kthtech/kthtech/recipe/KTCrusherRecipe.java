89fd3bed348fee292299698ca1fa140197b24102
StableAgOH <stagoh@qq.com>
2023-04-01 21:37:16 +0800

    temp

--- src/main/java/com/stagoh/kthtech/kthtech/recipe/KTCrusherRecipe.java
+++ src/main/java/com/stagoh/kthtech/kthtech/recipe/KTCrusherRecipe.java
@@ -1,8 +1,13 @@
 package com.stagoh.kthtech.kthtech.recipe;
 
+import org.jetbrains.annotations.Nullable;
+
+import com.google.gson.JsonObject;
+import com.stagoh.kthtech.kthtech.registry.KTRecipeSerializers;
 import com.stagoh.kthtech.kthtech.registry.KTRecipeTypes;
 
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
@@ -11,18 +16,25 @@ import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.crafting.CraftingHelper;
 
-public record KTCrusherRecipe(Ingredient input, int data, ItemStack output) implements Recipe<Container>
+public record KTCrusherRecipe(
+    ResourceLocation location,
+    Ingredient ingredient,
+    ItemStack result,
+    int time
+) implements Recipe<Container>
 {
+
     @Override
-    public boolean matches(Container p_44002_, Level p_44003_)
+    public boolean matches(Container container, Level level)
     {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'matches'");
     }
 
     @Override
-    public ItemStack assemble(Container p_44001_, RegistryAccess p_267165_)
+    public ItemStack assemble(Container container, RegistryAccess access)
     {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'assemble'");
@@ -36,29 +48,55 @@ public record KTCrusherRecipe(Ingredient input, int data, ItemStack output) impl
     }
 
     @Override
-    public ItemStack getResultItem(RegistryAccess p_267052_)
+    public ItemStack getResultItem(RegistryAccess access)
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getResultItem'");
+        return this.result;
     }
 
     @Override
     public ResourceLocation getId()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+        return this.location;
     }
 
     @Override
-    public RecipeSerializer<?> getSerializer()
+    public RecipeSerializer<KTCrusherRecipe> getSerializer()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSerializer'");
+        return KTRecipeSerializers.CRUSHER.get();
     }
 
     @Override
-    public RecipeType<?> getType()
+    public RecipeType<KTCrusherRecipe> getType()
     {
         return KTRecipeTypes.CRUSHER.get();
+    }
+
+    public static class Serializer implements RecipeSerializer<KTCrusherRecipe>
+    {
+        @Override
+        public KTCrusherRecipe fromJson(ResourceLocation location, JsonObject json)
+        {
+            var ingredient = Ingredient.fromJson(json.get("ingredient"));
+            var result = CraftingHelper.getItemStack(json.getAsJsonObject("result"), true, true);
+            var time = json.get("time").getAsInt();
+            return new KTCrusherRecipe(location, ingredient, result, time);
+        }
+
+        @Override
+        public @Nullable KTCrusherRecipe fromNetwork(ResourceLocation location, FriendlyByteBuf buf)
+        {
+            var ingredient = Ingredient.fromNetwork(buf);
+            var result = buf.readItem();
+            var time = buf.readInt();
+            return new KTCrusherRecipe(location, ingredient, result, time);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf buf, KTCrusherRecipe recipe)
+        {
+            recipe.ingredient.toNetwork(buf);
+            buf.writeItem(recipe.result);
+            buf.writeInt(recipe.time);
+        }
     }
 }
--- src/main/java/com/stagoh/kthtech/kthtech/registry/KTRecipeSerializers.java
+++ src/main/java/com/stagoh/kthtech/kthtech/registry/KTRecipeSerializers.java
@@ -1,6 +1,7 @@
 package com.stagoh.kthtech.kthtech.registry;
 
 import com.stagoh.kthtech.kthtech.KthTech;
+import com.stagoh.kthtech.kthtech.recipe.KTCrusherRecipe;
 
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -9,6 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(modid = KthTech.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class KTRecipeSerializers
@@ -16,6 +18,11 @@ public class KTRecipeSerializers
     public static final DeferredRegister<RecipeSerializer<?>> REGISTER = DeferredRegister.create(
         ForgeRegistries.RECIPE_SERIALIZERS,
         KthTech.MODID
+    );
+
+    public static final RegistryObject<RecipeSerializer<KTCrusherRecipe>> CRUSHER = REGISTER.register(
+        "crusher",
+        () -> new KTCrusherRecipe.Serializer()
     );
 
     @SubscribeEvent

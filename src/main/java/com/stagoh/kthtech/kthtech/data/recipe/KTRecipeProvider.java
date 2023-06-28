480d86e78ddc198f34092377fc6a991240355a0d
StableAgOH <stagoh@qq.com>
2023-06-28 13:35:43 +0800

    perf: remove test recipes

--- src/main/java/com/stagoh/kthtech/kthtech/data/recipe/KTRecipeProvider.java
+++ src/main/java/com/stagoh/kthtech/kthtech/data/recipe/KTRecipeProvider.java
@@ -1,7 +1,5 @@
 package com.stagoh.kthtech.kthtech.data.recipe;
 
-import static com.stagoh.kthtech.kthtech.util.KTUtils.ktRLoc;
-
 import java.util.function.Consumer;
 
 import com.stagoh.kthtech.kthtech.data.recipe.builder.KTCrusherRecipeBuilder;
@@ -9,13 +7,8 @@ import com.stagoh.kthtech.kthtech.registry.KTItems;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
-import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraftforge.common.Tags;
 
 public class KTRecipeProvider extends RecipeProvider
 {
@@ -27,6 +20,7 @@ public class KTRecipeProvider extends RecipeProvider
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> consumer)
     {
+        /*
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.IRON_INGOT, 3)
             .group(getItemName(Items.IRON_INGOT))
             .requires(Tags.Items.RAW_MATERIALS_IRON)
@@ -41,7 +35,7 @@ public class KTRecipeProvider extends RecipeProvider
             .requires(KTItems.COKE.get())
             .unlockedBy(getHasName(Items.RAW_IRON), has(Items.RAW_IRON))
             .save(consumer, ktRLoc(getConversionRecipeName(Items.IRON_INGOT, KTItems.COKE.get())));
-
+        
         SimpleCookingRecipeBuilder.smelting(
             Ingredient.of(Items.COAL),
             RecipeCategory.MISC,
@@ -50,6 +44,7 @@ public class KTRecipeProvider extends RecipeProvider
             200
         ).unlockedBy(getHasName(Items.COAL), has(Items.COAL))
             .save(consumer);
+        */
 
         KTCrusherRecipeBuilder.crusher(Items.RAW_IRON, KTItems.CRUSHED_RAW_IRON.get())
             .unlockedBy(getHasName(Items.RAW_IRON), has(Items.RAW_IRON))
